ad9d2cd61fc7e431d582d1f7f610cdb0375f584b
Nguyen Thi Thuy Nga <thuynga.ngth@gmail.com>
2018-01-13 23:04:23 +0700

    register.blade.php

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -11,55 +11,39 @@
 				<li class="women">ĐĂNG KÍ</li>
 			</ul>
 			<ul class="previous">
-				<li><a href="index.html">Trở về trang vừa truy cập</a></li>
+				<li><a href="index.html">QUAY LẠI</a></li>
 			</ul>
 			<div class="clearfix"></div>
 		</div>
-		<h2>ĐĂNG KÍ</h2>
+		
 		<div class="registration-grids">
 			<div class="reg-form">
 				<div class="reg">
-					<p>Welcome, please enter the following details to continue.</p>
-					<p>If you have previously registered with us, <a href="#">click here</a></p>
+					<h2>ĐĂNG KÍ</h2>
 					<form>
 						<ul>
-							<li class="text-info">First Name: </li>
-							<li><input type="text" value=""></li>
-						</ul>
-						<ul>
-							<li class="text-info">Last Name: </li>
-							<li><input type="text" value=""></li>
-						</ul>				 
+							<li class="text-info">Họ và tên </li>
+							<li><input type="text" value="" placeholder="Vui lòng nhập họ và tên"></li>
+						</ul>			 
 						<ul>
 							<li class="text-info">Email: </li>
-							<li><input type="text" value=""></li>
+							<li><input type="text" value="" placeholder="Vui lòng nhập Email"></li>
 						</ul>
 						<ul>
-							<li class="text-info">Password: </li>
-							<li><input type="password" value=""></li>
+							<li class="text-info">Mật khẩu: </li>
+							<li><input type="password" value="" placeholder="Vui lòng nhập mật khẩu"></li>
 						</ul>
 						<ul>
-							<li class="text-info">Re-enter Password:</li>
-							<li><input type="password" value=""></li>
+							<li class="text-info">Nhập lại mật khẩu:</li>
+							<li><input type="password" value="" placeholder="Vui lòng nhập lại mật khẩu"></li>
 						</ul>
-						<ul>
-							<li class="text-info">Mobile Number:</li>
-							<li><input type="text" value=""></li>
-						</ul>						
-						<input type="submit" value="REGISTER NOW">
-						<p class="click">By clicking this button, you are agree to my  <a href="#">Policy Terms and Conditions.</a></p> 
+						<p class="click">*Tôi đã đọc và đồng ý với <a href="#">chính sách </a> của M &amp; A Shop</p>
+						<br> 			
+						<input type="submit" value="ĐĂNG KÝ">
 					</form>
 				</div>
 			</div>
-			<div class="reg-right">
-				<h3>Completely Free Account</h3>
-				<div class="strip"></div>
-				<p>Pellentesque neque leo, dictum sit amet accumsan non, dignissim ac mauris. Mauris rhoncus, lectus tincidunt tempus aliquam, odio 
-				libero tincidunt metus, sed euismod elit enim ut mi. Nulla porttitor et dolor sed condimentum. Praesent porttitor lorem dui, in pulvinar enim rhoncus vitae. Curabitur tincidunt, turpis ac lobortis hendrerit, ex elit vestibulum est, at faucibus erat ligula non neque.</p>
-				<h3 class="lorem">Lorem ipsum dolor.</h3>
-				<div class="strip"></div>
-				<p>Tincidunt metus, sed euismod elit enim ut mi. Nulla porttitor et dolor sed condimentum. Praesent porttitor lorem dui, in pulvinar enim rhoncus vitae. Curabitur tincidunt, turpis ac lobortis hendrerit, ex elit vestibulum est, at faucibus erat ligula non neque.</p>
-			</div>
+
 			<div class="clearfix"></div>
 		</div>
 	</div>
