a2ac331c421bb95efc2b7997ef8c627d4644f10a
Duy-Nguyen-Van <vanduy1309@gmail.com>
2018-01-14 10:30:39 +0700

    Edited-ContactPage

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -20,7 +20,8 @@
 			<h2>LIÊN HỆ CHÚNG TÔI TẠI ĐÂY</h2>
 		</div>
 		<div class="contact-map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6632.248000703498!2d151.265683!3d-33.7832959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12abc7edcbeb07%3A0x5017d681632bfc0!2sManly+Vale+NSW+2093%2C+Australia!5e0!3m2!1sen!2sin!4v1433329298259" style="border:0"></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15672.854527397385!2d106.80203855633897!3d10.87134970217915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xafa66f9c8be3c91!2sUniversity+of+Information+Technology+VNU-HCM!5e0!3m2!1sen!2s!4v1515900507031" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			{{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6632.248000703498!2d151.265683!3d-33.7832959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12abc7edcbeb07%3A0x5017d681632bfc0!2sManly+Vale+NSW+2093%2C+Australia!5e0!3m2!1sen!2sin!4v1433329298259" style="border:0"></iframe> --}}
 		</div>
 		<div class="contact-form">
 			<div class="contact-info">
